6df71b4bc199446a1b212f291a1e5a8fb49db57e
Pushkar Raj <px86@protonmail.com>
2024-08-18 11:31:07 +0530

    Initial commit

---
+++ src/main/java/io/github/px86/spring_boot_blogging_app/SpringBootBloggingAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.px86.spring_boot_blogging_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBloggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBloggingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/px86/spring_boot_blogging_app/SpringBootBloggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.px86.spring_boot_blogging_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBloggingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
